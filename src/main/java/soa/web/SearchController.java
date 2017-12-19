e81a91e0dedd6c246bb339fd0d27486c8f37626f
crzyivo <ivan.nadie@gmail.com>
2017-12-19 17:51:05 +0100

    Added command *max:n* to search query

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -1,11 +1,15 @@
 package soa.web;
 
 import org.apache.camel.ProducerTemplate;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -19,10 +23,31 @@ public class SearchController {
         return "index";
     }
 
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(SearchController.class);
 
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        String[] parse = q.split("max:");
+        String qr = parse[0];
+        String n = "30";
+        if(parse.length != 1) {
+            String[] parse2 = parse[1].split(" ");
+            try {
+               Integer.parseInt(parse2[0]);
+               n = parse2[0];
+            }catch (NumberFormatException e){ }
+            if(parse2.length != 1) {
+                qr = qr + " " + parse2[1];
+            }
+        }
+        logger.info("qr: "+ qr);
+        logger.info("n: "+ n);
+        Map headers = new HashMap<>();
+        headers.put("CamelTwitterKeywords",qr);
+        headers.put("CamelTwitterCount",n);
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
+
+
 }
